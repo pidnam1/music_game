4d2bc43619f76de3fd95e5d6f447b29c57a8fed5
pidnam1 <pidnam1@gmail.com>
2021-06-26 15:53:16 -0400

    first

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Settings from "./Components/Settings";
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Music Game </h1>
+      <Settings />
+    </div>
+  );
+}
+
+export default App;
